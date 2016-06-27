129003a132a4b20a23d912b3a2d53d1d7ac32e27
Ryan Vanderwerf <rvanderwerf@gmail.com>
2016-06-26 22:46:18 -0500

    make work questions after number optional

--- src/main/groovy/com/vanderfox/hero/HeroQuizDomain.groovy
+++ src/main/groovy/com/vanderfox/hero/HeroQuizDomain.groovy
@@ -1,10 +1,14 @@
 package com.vanderfox.hero
 
+import grails.persistence.Entity
+
 /**
  * Created by rvanderwerf on 6/15/16.
  */
-class HeroQuizDomain {
+@Entity
+class HeroQuizDomain implements Serializable {
     int id
     String quizName
+    static mapWith = "dynamodb"
 
 }
--- src/main/groovy/com/vanderfox/hero/HeroQuizItemDomain.groovy
+++ src/main/groovy/com/vanderfox/hero/HeroQuizItemDomain.groovy
@@ -1,12 +1,16 @@
 package com.vanderfox.hero
 
+import grails.persistence.Entity
+
 /**
  * Created by rvanderwerf on 6/15/16.
  */
-class HeroQuizItemDomain {
+@Entity
+class HeroQuizItemDomain implements Serializable{
     HeroQuizDomain quiz
     String question
     String domain
     int id
+    static mapWith = "dynamodb"
 
 }
