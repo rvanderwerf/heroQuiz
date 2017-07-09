4a95728255080c6f410d0883c28af07af09645bb
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2017-07-09 00:21:50 -0500

    serialization stuff

--- src/main/groovy/com/vanderfox/hero/question/Question.groovy
+++ src/main/groovy/com/vanderfox/hero/question/Question.groovy
@@ -3,11 +3,15 @@ package com.vanderfox.hero.question
 /**
  * Created by lfox on 6/16/16.
  */
-class Question {
+class Question implements Serializable {
+    private static final long serialVersionUID = 1L; // try never to change
+
     String question
     String[] options
     int answer
     int index
+    String speechTtext
+    String cardText
 
     String getSpeechText() {
         String speechText = ""
